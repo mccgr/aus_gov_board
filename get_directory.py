13f320a015325385a670deff57e500adcf497488
Ian Gow <iandgow@gmail.com>
2018-04-05 07:38:32 -0400

    Clean-up of directory code.
    - Closes #2 and #6.

--- get_directory.py
+++ get_directory.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3 
-
-def is_integer(input):
-    try:
-        num = int(input)
-    except ValueError:
-        return False
-    except TypeError:
-        return False
-    return True
-
 from sqlalchemy import create_engine
 
 import os
@@ -49,88 +39,78 @@ class XML2DataFrame:
         return pd.DataFrame(structure_data)
 
 xml2df = XML2DataFrame(xml_data)
+
 df = xml2df.process_data()
 
-
-                     
+# Rearrange columns
+import numpy as np
 
 # Arrange the columns so that those with the least missing values appear on the left.
-
 indices = np.argsort(df.isnull().sum())
 df = df.iloc[:, indices]
 
-# Arrange the first few columns so that we have, in order, unique_record_id, content_id, updated, and type as
+# Arrange the first few columns so that we have, in order, 
+# unique_record_id, content_id, updated, and type as
 # the first four columns. 
+df = df.iloc[:, [1, 3, 0, 2] + list(range(4, len(df.columns)))]
 
-indices = [1, 3, 0, 2] + [i for i in range(4, 81)]
-df2 = df2.iloc[:, indices]
-df2.head()
-                     
-                     
+# Fix date columns
+
 # Let's start to put the columns which have dates into datetime format
-# Get rid of the "Determined by the appointer" entries in end_date by setting these entries to None
- 
-df.loc[np.logical_and(df['end_date'] == "Determined by the appointer", df['end_date'].notnull()), 'end_date'] = None 
+# Get rid of the "Determined by the appointer" entries in end_date by 
+# setting these entries to None
+df.loc[df['end_date'] == "Determined by the appointer", "end_date"] = None
 
+# For the other date columns, start_date, end_date and creation_date, 
+# let's convert to datetime format in the utc timezone, 
+# assuming the datetimes are currently in Sydney time.
+from pytz import timezone, utc
 
+def local_to_utc(x):
+    x.replace(tzinfo = timezone('Australia/Sydney')).astimezone(utc)
+    return(x)
 
-# For the other date columns, start_date, end_date and creation_date, let's convert to datetime format in the utc timezone.
-import datetime as dt
-from dateutil.tz import *
-import pytz
- 
-local = tzlocal()
-
-df['updated'] = pd.to_datetime(df['updated']).apply(lambda x: x.replace(tzinfo = local).astimezone(pytz.utc))
+df['updated'] = pd.to_datetime(df['updated']).apply(local_to_utc)
 df['start_date'] = pd.to_datetime(df['start_date'])
 df['end_date'] = pd.to_datetime(df['end_date'])
-df['creation_date'] = pd.to_datetime(df['creation_date'])
-                     
-                     
-                     
+df['creation_date'] = pd.to_datetime(df['creation_date'])                 
+
 # Now consider all the columns that should be in integer format
+def is_integer(input):
+    try:
+        num = int(input)
+    except ValueError:
+        return False
+    except TypeError:
+        return False
+    return True
 
-int_col_names = []
+def col_is_integer(col):
+    return sum(not is_integer(x) for x in col.value_counts().index) == 0
 
-for colname in df.columns:
-    if(sum([not is_integer(x) for x in df[colname].value_counts().index]) == 0):
-        int_col_names.append(colname)
+int_col_names = [colname for colname in df.columns 
+                     if(col_is_integer(df[colname]))]
 
 df[int_col_names] = df[int_col_names].apply(pd.to_numeric)
-                     
-                     
-                     
+
+# Write data to database
+
+# This is rather slow!
 df.to_sql('directory', engine, schema="aus_gov_board",
                      if_exists="replace", index=False)                    
-                     
-                     
-                     
-# Do some database-related clean-up
-engine.execute(
-    """
-    SET search_path TO aus_gov_board;
-    
-    ALTER TABLE directory OWNER TO aus_gov_board;
 
+# Do some database-related clean-up
+engine.execute("""
+    SET search_path TO aus_gov_board;
+    ALTER TABLE directory OWNER TO aus_gov_board;
     GRANT SELECT ON directory TO aus_gov_board_access;
-    """)
+""")
     
 # Convert the float columns to integer
+for col in int_col_names:
+    engine.execute("""
+        ALTER TABLE aus_gov_board.directory
+        ALTER COLUMN %s TYPE integer;
+    """ % col)
 
-engine.execute(
-    """
-    ALTER TABLE directory
-    ALTER COLUMN importance TYPE integer
-    ALTER COLUMN role_belongs_to TYPE integer
-    ALTER COLUMN contact TYPE integer
-    ALTER COLUMN parent_organisation TYPE integer
-    ALTER COLUMN parent_directory_structure TYPE integer
-    ALTER COLUMN portfolio TYPE integer
-    ALTER COLUMN max_members TYPE integer
-    ALTER COLUMN current_budget_total_expenditure TYPE integer
-    ALTER COLUMN asl TYPE integer
-    ALTER COLUMN current_budget_total_appropriations TYPE integer
-    ALTER COLUMN parent_non_portfolio TYPE integer
-    ALTER COLUMN parent_board_non_board TYPE integer;
-    """)
-
+engine.dispose()
